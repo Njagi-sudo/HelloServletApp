649dfe5f1b0e21a75b8d4a9ff26f6871b80db58d
njagiiii <keshgrace62@gmail.com>
2025-06-15 22:37:22 +0300

    Initial commit

---
+++ src/main/java/com/example/hello/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.example.hello;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello, welcome to my first servlet!</h1>");
+    }
+}
+
