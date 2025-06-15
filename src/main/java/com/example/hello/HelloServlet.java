5aa240b05f42108189d78bd945a352d47f15b1fa
njagiiii <keshgrace62@gmail.com>
2025-06-15 22:55:36 +0300

    Better comments

--- src/main/java/com/example/hello/HelloServlet.java
+++ src/main/java/com/example/hello/HelloServlet.java
@@ -5,12 +5,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * A basic servlet that handles HTTP GET requests and displays a welcome message.
+ */
+
 public class HelloServlet extends HttpServlet {
+//    @param request TheHttpServlet Request object that contains the request the client made
+//    @params response the HttpServletResponse object used to send a response back to the client
+//    @params IOException if an input or output error occurs while handling the request
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
+//        set the content type of the response to HTML
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello, welcome to my first servlet!</h1>");
+        response.getWriter().println("<h1>Hello World!</h1>");
     }
 }
 
